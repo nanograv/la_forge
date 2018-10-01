ce8363f852497777c3cd68017ea3ca26fb5913ba
Hazboun6 <jeffrey.hazboun@gmail.com>
2018-10-01 09:51:50 -0700

    Generalize get_noise_params to any 2 pars. Add docstrings.

--- la_forge/utils.py
+++ la_forge/utils.py
@@ -17,17 +17,50 @@ fyr = 1./31536000.
 
 def getMax2d(samples1, samples2, weights=None, smooth=True, bins=[40, 40],
             x_range=None, y_range=None, logx=False, logy=False, logz=False):
+    """ Function to return the maximum likelihood values by interpolating over
+    a two dimensional histogram made of two sets of samples.
+
+    Parameters
+    ----------
+
+    samples1, samples2 : array or list
+        Arrays or lists from which to find two dimensional maximum likelihood
+        values.
+
+    weights : array of floats
+        Weights to use in histogram.
+
+    bins : list of ints
+        List of 2 integers which dictates number of bins for samples1 and
+        samples2.
+
+    x_range : tuple, optional
+        Range of samples1
+
+    y_range : tuple, optional
+        Range of samples2
+
+    logx : bool, optional
+        A value of True use log10 scale for samples1.
+
+    logy : bool, optional
+        A value of True use log10 scale for samples2.
+
+    logz : bool, optional
+        A value of True indicates that the z axis is in log10.
+
+    """
 
     if x_range is None:
-        xmin = np.min(samples1)
-        xmax = np.max(samples1)
+        xmin = np.amin(samples1)
+        xmax = np.amax(samples1)
     else:
         xmin = x_range[0]
         xmax = x_range[1]
 
     if y_range is None:
-        ymin = np.min(samples2)
-        ymax = np.max(samples2)
+        ymin = np.amin(samples2)
+        ymax = np.amax(samples2)
     else:
         ymin = y_range[0]
         ymax = y_range[1]
@@ -44,11 +77,7 @@ def getMax2d(samples1, samples2, weights=None, smooth=True, bins=[40, 40],
     extent = [xedges[0], xedges[-1], yedges[0], yedges[-1] ]
 
     if logz:
-        for ii in range(hist2d.shape[0]):
-            for jj in range(hist2d.shape[1]):
-                if hist2d[ii,jj] <= 0:
-                    hist2d[ii,jj] = 1
-
+        hist2d = np.where(hist2d >= 0,hist2d,1)
 
     xedges = np.delete(xedges, -1) + 0.5*(xedges[1] - xedges[0])
     yedges = np.delete(yedges, -1) + 0.5*(yedges[1] - yedges[0])
@@ -68,20 +97,35 @@ def getMax2d(samples1, samples2, weights=None, smooth=True, bins=[40, 40],
     return xedges[ind[0]], yedges[ind[1]]
 
 
-def get_noise_params(core, pulsar):
+def get_noise_params_2d_mlv(core, par1, par2):
+    """Convenience function for finding two dimensional maximum likelihood
+    value for any two parameters.
+    """
 
-    # load individual chains and find 2d maxLL values for RN parameters
-    # pars = core.params
-    # chain = core.chain
-    burn = core.burn
-    
-    RN_spectral_index_common, RN_Amplitude_common = getMax2d(core.get_param(pulsar + '_gamma')[core.burn:],
-                                                             core.get_param(pulsar + '_log10_A')[core.burn:])
+    samps1 = core.get_param(par1, to_burn=True)
+    samps2 = core.get_param(par2, to_burn=True)
 
-    return RN_Amplitude_common, RN_spectral_index_common
+    return getMax2d(samps1,samps2)
 
+def get_rn_noise_params_2d_mlv(core, pulsar):
+    """Convenience function to find 2d rednoise maximum likelihood values.
+    """
+    rn_si = pulsar + '_gamma'
+    rn_amp = pulsar + '_log10_A'
+
+    return get_noise_params_2d_mlv(rn_si,rn_amp)
 
 def get_Tspan(pulsar, datadir):
+    """Returns timespan of a pulsars dataset by loading the pulsar as an
+    `enterprise.Pulsar()` object.
+
+    Parameters
+    ----------
+    pulsar : str
+
+    datadir : str
+        Directory where `par` and `tim` files are found.
+    """
     if not ent_present:
         raise ImportError('enterprise is not available for import. '
                           'Please provide time span of data in another form.')
@@ -95,7 +139,10 @@ def get_Tspan(pulsar, datadir):
     return T
 
 
-# converts from power to residual RMS
+
 def compute_rho(log10_A, gamma, f, T):
+    """
+    Converts from power to residual RMS.
+    """
 
     return np.sqrt((10**log10_A)**2 / (12.0*np.pi**2) * fyr**(gamma-3) * f**(-gamma) / T)

66df6d785332cc1374922c1287be9e54cb97e8e4
Hazboun6 <jeffrey.hazboun@gmail.com>
2019-02-03 14:38:55 -0800

    Testing.

--- la_forge/__init__.py
+++ la_forge/__init__.py
@@ -7,9 +7,9 @@ from __future__ import (absolute_import, division,
 __author__ = """Jeffrey Shafiq Hazbboun"""
 __email__ = 'jeffrey.hazboun@gmail.com'
 __version__ = '0.1.0'
-
-from .rednoise import plot_rednoise_spectrum
-from . import utils
-from .core import Core
-
-__all__ = ['utils','core','slices','rednoise','diagnostics','gp']
+#
+# from .rednoise import plot_rednoise_spectrum
+# from . import utils
+# from .core import Core
+#
+# __all__ = ['utils','core','slices','rednoise','diagnostics','gp']

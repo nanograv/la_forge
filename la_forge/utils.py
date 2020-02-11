40453ab61b701c83ad64cc6051467f18dacc16a6
Hazboun6 <jeffrey.hazboun@gmail.com>
2020-02-11 13:23:23 -0800

    Added function for weighted quantiles.

--- la_forge/utils.py
+++ la_forge/utils.py
@@ -148,6 +148,7 @@ def compute_rho(log10_A, gamma, f, T):
 
     return np.sqrt((10**log10_A)**2 / (12.0*np.pi**2)
                     * fyr**(gamma-3) * f**(-gamma) / T)
+                    
 
 def convert_pal2_pars(p2_par):
     p2 = p2_par.split('_')
@@ -229,3 +230,39 @@ def powerlaw(freqs, log10_A=-16, gamma=5):
     df = np.diff(np.concatenate((np.array([0]), freqs)))
     return ((10**log10_A)**2 / 12.0 / np.pi**2 *
             fyr**(gamma-3) * freqs**(-gamma) * np.repeat(df, 2))
+
+
+def weighted_quantile(values, quantiles, sample_weight=None,
+                      values_sorted=False, old_style=False):
+    """ Very close to numpy.percentile, but supports weights.
+    NOTE: quantiles should be in [0, 1]!
+    [From Max Ghenis via Stack Overflow:
+     https://stackoverflow.com/questions/21844024/weighted-percentile-using-numpy]
+    :param values: numpy.array with data
+    :param quantiles: array-like with many quantiles needed
+    :param sample_weight: array-like of the same length as `array`
+    :param values_sorted: bool, if True, then will avoid sorting of
+        initial array
+    :param old_style: if True, will correct output to be consistent
+        with numpy.percentile.
+    :return: numpy.array with computed quantiles.
+    """
+    values = np.array(values)
+    quantiles = np.array(quantiles)
+    if sample_weight is None:
+        sample_weight = np.ones(len(values))
+    sample_weight = np.array(sample_weight)
+    assert np.all(quantiles >= 0) and np.all(quantiles <= 1), \
+        'quantiles should be in [0, 1]'
+    if not values_sorted:
+        sorter = np.argsort(values)
+        values = values[sorter]
+        sample_weight = sample_weight[sorter]
+    weighted_quantiles = np.cumsum(sample_weight) - 0.5 * sample_weight
+    if old_style:
+        # To be convenient with numpy.percentile
+        weighted_quantiles -= weighted_quantiles[0]
+        weighted_quantiles /= weighted_quantiles[-1]
+    else:
+        weighted_quantiles /= np.sum(sample_weight)
+    return np.interp(quantiles, weighted_quantiles, values)

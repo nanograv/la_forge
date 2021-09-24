5a08e18902cd7eb09833b086fd9be8d714419635
Hazboun6 <jeffrey.hazboun@gmail.com>
2021-09-23 21:50:47 -0700

    first real tests

--- tests/test_la_forge.py
+++ tests/test_la_forge.py
@@ -4,7 +4,7 @@
 """Tests for `la_forge` package."""
 
 import pytest
-
+import os
 
 from la_forge import core
 from la_forge import diagnostics
@@ -12,18 +12,32 @@ from la_forge import rednoise
 from la_forge import slices
 from la_forge import utils
 
+testdir = os.path.dirname(os.path.abspath(__file__))
+datadir = os.path.join(testdir, 'data')
+
+chaindir = os.path.join(datadir, 'chains', 'adv_noise_J1713+0747','')
+plaw_core_path = os.path.join(datadir, 'cores', 'J1713+0747_plaw_dmx.core')
+fs_core_path = os.path.join(datadir, 'cores', 'J1713+0747_fs_dmx.core')
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
+def plaw_core():
+    """Premade Free Spectral Core.
+    J1713+0747 Free Spectral Noise run, NG12.5 yr dataset.
     """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    return core.load_Core(plaw_core_path)
+
+@pytest.fixture
+def fs_core():
+    """Premade Power Law Red Noise Core.
+    J1713+0747 Power Law Red Noise run, NG12.5 yr dataset
+    """
+    return core.load_Core(fs_core_path)
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_core():
+    """Tests the loading of a Core into a class. """
+    c0=core.Core(label='J1713+0747 Adv Noise Modeling Round 3a',
+                 chaindir=chaindir)
+
+    assert hasattr(c0,'get_param')
+    assert hasattr(c0,'params')

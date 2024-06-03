af7619a4d3329ce8d0473a5b58e443cf38456372
Jeremy Baier <baierj@oregonstate.edu>
2024-06-02 21:17:42 -0700

    fixing an indent

--- la_forge/core.py
+++ la_forge/core.py
@@ -691,7 +691,7 @@ class HyperModelCore(Core):
         """
         # Call to add `param_dict` and `log_weights` to dictionaries for hdf5 to search for.
         self.param_dict = param_dict
-	self.log_weights = log_weights
+        self.log_weights = log_weights
         super()._set_hdf5_lists(append=[('param_dict', '_savedicts')])
         super()._set_hdf5_lists(append=[('log_weights', '_savedicts')])
         super().__init__(chaindir=chaindir, burn=burn,
